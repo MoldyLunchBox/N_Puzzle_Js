63a6ca01d050b31856e07d95f685e956072a5afc
Abderrahmane Mya <amya@e1r6p1.1337.ma>
2023-04-01 01:03:41 +0000

    substates generator

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { BloomFilter } from "bloomfilter";
 import { getNeighbors } from "./utils/utils";
+import {Node} from "./utils/node"
+const {log} = console
 function main() {
-	const initState = [1,0,2,3,4,5,6,7,8]
+	const initState = [ [ '1', '0', '2' ], [ '3', '4', '5' ], [ '6', '7', '8' ] ]
+	const node = new Node(initState, null)
+	node.getSubStates()
+	log(node.subStates)
 	const bloomFilter = new BloomFilter(32 * 1024 * 40000, 32);
-	getNeighbors(initState)
 }
 main()
 
--- src/utils/node.js
+++ src/utils/node.js
@@ -1,3 +1,62 @@
-export class Node{
-    constructor(state, parent, action, score){}
+
+
+function duplicateArray(arr) {
+    return arr.map(function(row) {
+      return row.slice();
+    });
+  }
+function createSubState(state, emptyIndex, direction) {
+    const newState = duplicateArray(state)
+    console.log(direction)
+    switch (direction) {
+        case 'left':
+            newState[emptyIndex.y][emptyIndex.x] = newState[emptyIndex.y][emptyIndex.x - 1]
+            newState[emptyIndex.y][emptyIndex.x - 1] = '0'
+            return newState
+        case 'right':
+            newState[emptyIndex.y][emptyIndex.x] = newState[emptyIndex.y][emptyIndex.x + 1]
+            newState[emptyIndex.y][emptyIndex.x + 1] = '0'
+            return newState
+        case 'down':
+            newState[emptyIndex.y][emptyIndex.x] = newState[emptyIndex.y + 1][emptyIndex.x]
+            newState[emptyIndex.y + 1][emptyIndex.x] = '0'
+            return newState
+        case 'up':
+            newState[emptyIndex.y][emptyIndex.x] = newState[emptyIndex.y - 1][emptyIndex.x]
+            newState[emptyIndex.y - 1][emptyIndex.x] = '0'
+            return newState
+    }
+}
+function getIndex(state, target) {
+    let rowIndex = -1;
+    let colIndex = -1;
+
+    for (let i = 0; i < state.length; i++) {
+        const row = state[i];
+        if (row.indexOf(target) !== -1) {
+            rowIndex = i;
+            colIndex = row.indexOf(target);
+            break
+        }
+    }
+    return ({ y: rowIndex, x: colIndex })
+}
+export class Node {
+    constructor(state, parent) {
+        this.state = state
+        this.parent = parent
+        this.mapSize = state.length
+        this.subStates = []
+    }
+    getSubStates() {
+        const emptyIndex = getIndex(this.state, '0')
+        if (emptyIndex.x > 0)
+            this.subStates.push(createSubState(this.state, emptyIndex, "left"))
+        if (emptyIndex.x < this.mapSize)
+            this.subStates.push(createSubState(this.state, emptyIndex, "right"))
+        if (emptyIndex.y < this.mapSize)
+            this.subStates.push(createSubState(this.state, emptyIndex, "down"))
+        if (emptyIndex.y > 0)
+            this.subStates.push(createSubState(this.state, emptyIndex, "up"))
+    }
 }
